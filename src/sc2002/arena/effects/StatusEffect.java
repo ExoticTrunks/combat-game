e445bcc35a913c6f2faa1688ea2e952ef2d85346
kirtish <kirtishjayaprasad@gmail.com>
2026-03-27 20:27:31 +0800

    Update StatusEffect.java

--- src/sc2002/arena/effects/StatusEffect.java
+++ src/sc2002/arena/effects/StatusEffect.java
@@ -1,1 +1,11 @@
 // this will be the interface for all the effects to be built upon
+package sc2002.arena.effects;
+public interface StatusEffect {
+    String getName();
+    void tick();
+    boolean isExpired();
+    default int getAttackModifier() { return 0; }
+    default int getDefenseModifier() { return 0; }
+    default boolean preventsAction() { return false; }
+    default boolean grantsInvulnerability() { return false; }
+}
