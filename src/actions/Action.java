f14639b030609b863500ff61de45d3d294aadfd3
kirtish <kirtishjayaprasad@gmail.com>
2026-03-29 19:52:10 +0800

    Update Action.java

--- src/actions/Action.java
+++ src/actions/Action.java
@@ -1,1 +1,8 @@
-//this will be the enum class for the actions in the game
+package action;
+import model.Combatant; //yet to be implimented but this is the rough idea
+import java.util.List;
+
+public interface Action {
+    String execute(Combatant actor, List<Combatant> targets); // try this first and then see if it holds up later on
+    String getName();
+}
