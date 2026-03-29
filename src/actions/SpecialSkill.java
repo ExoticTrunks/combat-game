4970398d2f9a1d0ab74bda1ba2fdab3a125862b2
kirtish <kirtishjayaprasad@gmail.com>
2026-03-29 19:56:05 +0800

    Create SpecialSkill.java

---
+++ src/actions/SpecialSkill.java
@@ -0,0 +1,20 @@
+package action;
+// the idea behind using abstract right now is so we don't have to impliment the execute() method yet
+public abstract class SpecialSkill implements Action {
+    private String name;
+    private String description;
+
+    public SpecialSkill(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
