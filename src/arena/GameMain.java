6d764577e98462c2f4b05f6b8debac24c822f1ed
ExoticTrunks <83502407+ExoticTrunks@users.noreply.github.com>
2026-04-05 22:29:56 +0800

    Add GameMain class to start the game

---
+++ src/arena/GameMain.java
@@ -0,0 +1,9 @@
+package com.ntu.sc2002.arena;
+
+import com.ntu.sc2002.arena.ui.GameCLI;
+
+public class GameMain {
+    public static void main(String[] args) {
+        new GameCLI().run();
+    }
+}
